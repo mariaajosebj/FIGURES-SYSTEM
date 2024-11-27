ddafb35887d1f59409c8d7c4ddb50aeb7a8fd335
mariaajosebj <mbarzolaj@est.ups.edu.ec>
2024-11-27 08:48:27 -0500

    Add(I): Square Implementation

--- src/figures/Square.java
+++ src/figures/Square.java
@@ -1,4 +1,18 @@
 package figures;
 
 public class Square {
+    private int length;
+
+    public Square() {}
+    public Square(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
