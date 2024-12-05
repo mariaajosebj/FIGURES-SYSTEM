e3e435f8e0b3aac7559adae9e2ab866725ec4bae
mariaajosebj <mbarzolaj@est.ups.edu.ec>
2024-12-04 21:31:01 -0500

    Add (I): Add square, draw and calculate

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import figures.Line;
+import figures.Square;
 
 import java.util.Scanner;
 
@@ -6,29 +7,39 @@ public class Main {
     public static void main(String[] args) {
         int option = 0;
         Scanner scanner = new Scanner(System.in);
+        Line line = new Line();
+        Square square = new Square();
         do {
-            Line line = new Line();
             System.out.println("------ Figures System ------");
             System.out.println("1. Create Line");
-            System.out.println("2. Print Line");
-            System.out.println("3. Exit");
+            System.out.println("2. Create Square");
+            System.out.println("3. Print Line");
+            System.out.println("4. Print Square");
+            System.out.println("5. Exit");
             option = scanner.nextInt();
             switch (option) {
                 case 1:
-                    System.out.println("Please enter line length: ");
+                    System.out.println("Please enter Line length: ");
                     int lineLength = scanner.nextInt();
                     line.setLength(lineLength);
                     break;
                 case 2:
-                    line.drawLine();
+                    System.out.println("Please enter Square length");
+                    int SquareLength = scanner.nextInt();
+                    square.setLength(SquareLength);
                     break;
                 case 3:
+                    line.drawLine();
+                    break;
+                case 4:
+                    square.PrintSquare();
+                case 5:
                     System.out.println("------ Exiting System -------");
                     break;
                 default:
                     System.out.println("Please enter a valid option");
                     break;
             }
-        }while (option != 3);
+        }while (option != 5);
     }
 }
