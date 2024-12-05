8a82c98d09a99ae7c0b3eb6ea333a6fa00987c9c
mariaajosebj <mbarzolaj@est.ups.edu.ec>
2024-12-04 21:31:01 -0500

    Add(I): Square Implementation

--- src/figures/Square.java
+++ src/figures/Square.java
@@ -4,8 +4,36 @@ public class Square {
     private int length;
 
     public Square() {}
+
     public Square(int length) {
         this.length = length;
+    }
+
+    public void drawSquare() {
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    public int calculatePerimeter() {
+        return 4 * length;
+    }
+
+    public int calculateArea() {
+        return length * length;
+    }
+
+    public void PrintSquare() {
+        System.out.println("Drawing a Square with length: " + length + "\n");
+        drawSquare();
+        System.out.println("Perimeter: " + length + " * 4 = " + calculatePerimeter());
+        System.out.println("Area: " + length + " * " + length + " = " + calculateArea());
+    }
+
+    public int CalculatePerimeter() {
+        return length * 4;
     }
 
     public int getLength() {
